cc7fe5ef9c6faae2e429c34d44cbede0e8866d85
Brian Purgert <brianpurgert2@gmail.com>
2023-02-18 05:45:57 -0500

    small timeline changes nuxt versions

--- components/ProjectsTimeline.vue
+++ components/ProjectsTimeline.vue
@@ -1,26 +1,23 @@
-
+<!--
+  2004	Battlefield 1942 modding
+  2006	Scripting
+  2008	Crysis Interstate
+  2010 	Space Bike
+  2012 	Project High Flight
+  2014 	Smart Diaper
+  2017  SimpleGSA
+  2018 	Ruby Discord
+  2019 	Twixer Chrome Extension
+  -->
 
 <template>
-  <v-timeline align="start" line-inset="12">
-      <v-timeline-item dot-color="cyan" size="small">
-          <template v-slot:opposite>
-              <div class="pt-1 headline font-weight-bold text-cyan" v-text="1992"></div>
-          </template>
-          <div>
-              <h2 :class="`mt-n1 headline font-weight-light mb-4 text-amber`">
-                  heading
-              </h2>
-              <div>
-                  body
-              </div>
-          </div>
-      </v-timeline-item>
+<v-timeline>
       <v-timeline-item size="x-large" line-inset="25" class="twixer" dot-color="transparent">
           <template v-slot:icon>
               <v-avatar image="/twixer/twixer_icon.svg" size="130%" rounded="0"></v-avatar>
           </template>
           <template v-slot:opposite>
-              <div class="pt-1 headline font-weight-bold text-cyan" v-text="2000"></div>
+              <div class="pt-1 headline font-weight-bold text-cyan" v-text="2019"></div>
           </template>
           <v-card class="elevation-2">
               <v-card-title class="text-h5">
@@ -35,14 +32,16 @@
       </v-timeline-item>
       <v-timeline-item>
           <template v-slot:opposite>
-              <div class="pt-1 headline font-weight-bold text-cyan" v-text="2002"></div>
+              <div class="pt-1 headline font-weight-bold text-cyan" v-text="2018"></div>
           </template>
           <div>
               <h2 :class="`mt-n1 headline font-weight-light mb-4 text-amber`">
-                  heading
+                  <Icon name="logos:ruby" />
+                  <Icon name="logos:discord-icon" />
               </h2>
               <div>
-                  body
+
+
               </div>
           </div>
       </v-timeline-item>
