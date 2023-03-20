649aadf669f58a7f9f86266b663ff3bdfeec2d3f
Brian Purgert <brianpurgert2@gmail.com>
2023-03-20 12:40:09 -0400

    updates and bug fixes

--- components/ProjectsTimeline.vue
+++ components/ProjectsTimeline.vue
@@ -55,8 +55,14 @@
 						v-text = "2010"
 				></div>
 			</template>
+			<v-card class = "elevation-2" style = "min-width: 35vw!important;">
+				<!--				<img src = "/SpaceBike/rotating_photos.png" width = "854" height = "480" style = ''/>-->
+				<video autoplay loop muted playsinline>
+					<source src = "/doodlebike/DoodleBike_Portfolio_final.webm" type = "video/webm"/>
 
-			<Carousel :slides = "slides"></Carousel>
+				</video>
+			</v-card>
+			<!--			<Carousel :slides = "slides"></Carousel>-->
 
 
 		</v-timeline-item>
@@ -70,45 +76,6 @@
 			</template>
 			<v-container>
 				<v-sheet class = "elevation-2" style = "min-width: 35vw!important;">
-
-
-					<div style = "max-width: 853px">
-						<div style = "position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">
-							<iframe
-									src = "https://govconsvcs-my.sharepoint.com/personal/brianpurgert_govconsvcs_com/_layouts/15/embed.aspx?UniqueId=43627872-0795-40a9-865e-79b057e3fa07&embed=%7B%22hvm%22%3Atrue%2C%22ust%22%3Afalse%7D&referrer=OneUpFileViewer&referrerScenario=EmbedDialog.Create"
-									width = "853"
-									height = "480"
-									frameborder = "0"
-									scrolling = "no"
-									allowfullscreen
-									title = "desktop_2018_11_27___10_57_05_02_proxy.mp4"
-									style = "border:none; position: absolute; top: 0; left: 0; right: 0; bottom: 0; height: 100%; max-width: 100%;"></iframe>
-						</div>
-					</div>
-					<video
-							autoplay muted
-							width = "320"
-							height = "240"
-							controls
-							disablepictureinpicture
-							controlslist = "nodownload noplaybackrate">
-						>
-						<source
-								src = "https://firebasestorage.googleapis.com/v0/b/brianpurgert2/o/Doodle%20Bike_2018-06-25-09-24-52.mp4?alt=media&token=65ee4d06-2069-4d76-9c46-77ee7130f0c6"
-								type = "video/mp4">
-					</video>
-					<video
-							autoplay muted
-							width = "320"
-							height = "240"
-							controls
-							disablepictureinpicture
-							controlslist = "nodownload noplaybackrate">
-						>
-						<source
-								src = "https://govconsvcs-my.sharepoint.com/personal/brianpurgert_govconsvcs_com/_layouts/15/guestaccess.aspx?docid=01c33540ab0d74ba5b40cf2b65e0d6359&authkey=ASaNqecJBoQaOplGgifQhm0&e=vTjeFm"
-								type = "video/mp4">
-					</video>
 
 
 				</v-sheet>
