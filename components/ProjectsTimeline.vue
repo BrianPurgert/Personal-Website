3f4ca9b585fec6df17c7ca2c8d19cbc66ae3a02f
Brian Purgert <brianpurgert2@gmail.com>
2023-03-27 01:05:14 -0400

    media added

--- components/ProjectsTimeline.vue
+++ components/ProjectsTimeline.vue
@@ -25,50 +25,48 @@
 
 	<v-timeline
 			side = "end"
-			density = "comfortable"
+			density = "default"
 			line-inset = "28"
 			align = "center"
 	>
-		<v-timeline-item width = "100%">
+		<!--	IS82	-->
+		<v-timeline-item width = "min-content" min-width = "fit-content">
+
 			<v-container class = "">
-				<v-row justify = "space-around">
+				<v-card class = "elevation-6 ">
+					<v-card-title>Battlefield 1942 mapping</v-card-title>
+					<v-card-subtitle></v-card-subtitle>
+					<v-card-item>
+						<v-carousel
+								hide-delimiters
+								cycle
+								show-arrows = "hover"
+								progress = "primary"
+						>
+							<v-carousel-item
+									src = "/is82/2004.jpg"
+									aspect-ratio = "1.8"
+							></v-carousel-item>
 
-					<v-col cols = "10">
-						<v-card
-								min-width = "fit-content"
-								class = "elevation-6 justify-space-around">
-							<v-card-title>Battlefield 1942 mapping</v-card-title>
-							<v-card-subtitle></v-card-subtitle>
-							<v-card-item>
-								<v-carousel
-										hide-delimiters
-										cycle
-										show-arrows = "hover"
-										:height = "400"
+							<v-carousel-item
+									src = "/is82/2004_1.jpg"
+									aspect-ratio = "1.0"
+							></v-carousel-item>
 
-								>
-									<v-carousel-item
-											src = "/is82/2004.jpg"
-											contain
-									></v-carousel-item>
+							<v-carousel-item
+									src = "/is82/2004_2.jpg"
+									aspect-ratio = "1.8"
+							></v-carousel-item>
+						</v-carousel>
+					</v-card-item>
+					<v-card-text>
+						I started modding in Battlefield 1942 in 2004, when I was 12. specifically for a mod called interstate 82.
+						I was a perfectionist, with a strong desire to make the best map I could possible. unfortunately, my computer broke and I was never able to finish the map.
+					</v-card-text>
 
-									<v-carousel-item
-											src = "/is82/2004_1.jpg"
-											contain
-									></v-carousel-item>
-
-									<v-carousel-item
-											src = "/is82/2004_2.jpg"
-											contain
-									></v-carousel-item>
-								</v-carousel>
-							</v-card-item>
-							<v-card-text>Interstate 82</v-card-text>
-						</v-card>
-					</v-col>
+				</v-card>
 
 
-				</v-row>
 			</v-container>
 			<template v-slot:opposite>
 				<div
@@ -80,7 +78,8 @@
 				<img src = "/is82/backside.png" style = 'max-width: 80px'/>
 			</template>
 		</v-timeline-item>
-		<v-timeline-item>
+		<!--	RuneScape  	-->
+		<v-timeline-item width = "min-content" min-width = "fit-content">
 			<template v-slot:opposite>
 				<div
 						class = "bp-year"
@@ -90,16 +89,21 @@
 			<template v-slot:icon>
 				<nuxt-icon name = "scar_divi" filled style = 'font-size: 5em'/>
 			</template>
-			<v-card class = "elevation-2">
-				<v-card-title>SCAR Scripting</v-card-title>
-				<v-card-subtitle>my hello world</v-card-subtitle>
-				<v-card-text>
-					<nuxt-link href = "https://wiki.scar-divi.com/SCAR_Divi_Online_Manual">SCAR Wiki</nuxt-link>
-				</v-card-text>
-			</v-card>
+			<v-container>
+				<v-card class = "elevation-2">
+					<v-card-title>SCAR Scripting</v-card-title>
+					<v-card-subtitle>My first time writing code</v-card-subtitle>
+					<v-card-text>I was 13 when I learned
+						<nuxt-link href = "https://wiki.scar-divi.com/SCAR_Divi_Online_Manual">SCAR Divi</nuxt-link>
+					             to automate tasks in RuneScape for me. personally I created a bot that would automatically buy feathers and sell them
+					             back to the store owner, making me a profit. Not impressive, but it was my first time writing code, and I got hooked,
+					             I actually thought scripting in RuneScape was more fun than the game itself.
+					</v-card-text>
+				</v-card>
+			</v-container>
 		</v-timeline-item>
-
-		<v-timeline-item>
+		<!--    Crysis Interstate   -->
+		<v-timeline-item width = "min-content" min-width = "fit-content">
 			<template v-slot:opposite>
 				<div
 						class = "bp-year"
@@ -111,15 +115,31 @@
 				<nuxt-icon name = "cryengine-logo-vertical-white" filled style = 'font-size: 8em'/>
 				<!--								<img src = "/crysis_interstate/crysis_interstate_icon.png" style = 'max-width: 60px'/>-->
 			</template>
-			<v-card class = "elevation-2">
-				<v-card-title>Crysis Interstate</v-card-title>
-				<v-card-subtitle>Crysis Mod development</v-card-subtitle>
-				<v-card-text></v-card-text>
-			</v-card>
-
+			<v-container>
+				<v-card class = "elevation-2">
+					<v-card-title>Crysis Interstate</v-card-title>
+					<v-card-subtitle>Crysis Mod development</v-card-subtitle>
+					<v-card-text></v-card-text>
+				</v-card>
+			</v-container>
 		</v-timeline-item>
 		<!--	SpaceBike/Doodle Bike   -->
-		<v-timeline-item>
+		<v-timeline-item width = "min-content" min-width = "fit-content">
+			<v-container>
+				<v-card class = "elevation-5" width = "100%" style = "min-width: 35vw!important;">
+					<v-card-title>Doodle Bike</v-card-title>
+					<v-card-subtitle>Published on Google Play</v-card-subtitle>
+					<v-card-item>
+
+						<video-background
+								src = "/doodlebike/DoodleBike_Portfolio_final.webm"
+								style = "height: 30vh;"
+								objectFit = "cover"
+						>
+						</video-background>
+					</v-card-item>
+				</v-card>
+			</v-container>
 			<template v-slot:icon>
 				<img src = "/doodlebike/doodlebike_icon.png" style = 'max-width: 60px'/>
 			</template>
@@ -129,24 +149,28 @@
 						v-text = "2010"
 				></div>
 			</template>
-			<v-card class = "elevation-2" style = "min-width: 44vw!important;">
-				<video-background
-						src = "/doodlebike/DoodleBike_Portfolio_final.webm"
-						style = "max-height: 400px; height: 40vh;"
 
-						objectFit = "cover"
 
-				>
-				</video-background>
-
-			</v-card>
-			<v-card>
-
-			</v-card>
 		</v-timeline-item>
-		<v-timeline-item>
+		<!--	spacebike	-->
+		<v-timeline-item width = "min-content" min-width = "fit-content">
+			<v-container>
+				<v-card class = "elevation-2" width = "400" style = "min-width: 35vw!important;">
+					<v-card-title class = "text-h5">SpaceBike</v-card-title>
+					<v-card-subtitle>
+						<!--						<Icon name = "logos:google-play-icon" size = "0.8em"/>-->
+						Published on Google Play
+					</v-card-subtitle>
+					<v-card-item>
+						<video-background
+								src = "/SpaceBike/gameplay_space_bike_trimmed.mp4"
+								style = "height: 30vh;"
+
+						></video-background>
+					</v-card-item>
+				</v-card>
+			</v-container>
 			<template v-slot:icon>
-				<!--				<img src = "/SpaceBike/spacebike_icon.png" style = 'max-width: 60px'/>-->
 				<nuxt-icon filled name = "spacebike_icon_new" style = 'font-size: 5em'/>
 			</template>
 			<template v-slot:opposite>
@@ -155,22 +179,10 @@
 						v-text = "2010"
 				></div>
 			</template>
-			<v-container>
-				<v-sheet class = "elevation-2" style = "min-width: 35vw!important;">
-					<v-card-title class = "text-h5"></v-card-title>
-					<v-card-text></v-card-text>
-					<v-card-item>
-						<video-background
-								src = "/SpaceBike/gameplay_space_bike_trimmed.mp4"
-								style = "max-height: 400px; height: 40vh;"
-								objectFit = "cover"
-						></video-background>
-					</v-card-item>
-				</v-sheet>
-			</v-container>
+
 		</v-timeline-item>
 		<!--	SimpleGSA   -->
-		<v-timeline-item>
+		<v-timeline-item width = "min-content" min-width = "fit-content">
 			<template v-slot:opposite>
 				<div
 						class = "bp-year"
@@ -185,10 +197,11 @@
 			</v-container>
 		</v-timeline-item>
 		<!--	Ruby Discord   -->
-		<v-timeline-item dot-color = "transparent" line-inset = "25" size = "x-large">
+		<v-timeline-item
+				width = "min-content" min-width = "fit-content"
+				dot-color = "transparent" line-inset = "25" size = "x-large">
 			<template v-slot:icon>
 				<nuxt-icon filled name = "ruby_solo" style = 'font-size: 5em'/>
-
 			</template>
 			<template v-slot:opposite>
 				<div
@@ -196,29 +209,37 @@
 						v-text = "'2018'"
 				></div>
 			</template>
-			<v-card class = "elevation-2">
-				<v-card-title class = "text-h5 ">Ruby Discord</v-card-title>
-				<v-card-text>
-					<p>
-						Created the official Ruby Discord
-					</p>
-					<p>
+			<v-container>
+				<v-card
+						class = "mx-auto"
+						width = "400"
+				>
+					<v-img
+							src = "/RubyDiscord/ruby_discord.jpg"
+							height = "220px"
+							cover
+					>
+					</v-img>
+					<v-card-title class = "">Founded the Ruby Discord</v-card-title>
+
+
+					<v-card-actions>
 						<v-btn
-								color = "primary"
-								dark
 								href = "https://discord.com/servers/ruby-518658712081268738"
-
+								color = "orange-lighten-2"
+								variant = "text"
 						>
-
-
+							Server Webpage
 						</v-btn>
-					</p>
+					</v-card-actions>
 
-				</v-card-text>
-			</v-card>
+
+				</v-card>
+			</v-container>
 		</v-timeline-item>
 		<!--	Twixer   -->
 		<v-timeline-item
+				width = "min-content" min-width = "fit-content"
 				class = "twixer"
 				dot-color = "transparent"
 				line-inset = "25"
@@ -237,15 +258,22 @@
 						v-text = "2019"
 				></div>
 			</template>
-			<v-card
-					class = "elevation-2"
-			>
-				<v-card-title class = "text-h5">Twixer</v-card-title>
+			<v-container>
+				<v-card
+						class = "elevation-2" width = "800"
+				>
+					<v-img
+							src = "/twixer/description.png"
+							height = "520px"
+							cover
+					>
+					</v-img>
+					<v-card-title class = "text-h5">Twixer</v-card-title>
+					<v-card-subtitle>Chrome Extension</v-card-subtitle>
 
 
-				<!--					<img alt = "Sidebar" src = "/twixer/sidebar.png" >-->
-
-			</v-card>
+				</v-card>
+			</v-container>
 		</v-timeline-item>
 	</v-timeline>
 </template>
