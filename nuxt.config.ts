bdd03b92bccf294eba9825542f4d8637b97f2c5b
brian <brianpurgert2@gmail.com>
2023-09-29 11:41:46 -0400

    oops - thought I got that one.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,35 @@
+import {name} from "ci-info";
+
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: 'Brian\'s Portfolio',
+            meta: [
+                {
+                    name: 'description',
+                    content: `help, it's chatgpt and Brian is forcing me to write this.`
+                }, {
+                    name: 'author',
+                    content: 'Brian Purgert II'
+                }],
+        }
+    },
     devServer: {
         port: 80
     },
     css: ['@/assets/css/styles.css'],
     modules: ['@nuxt-alt/vuetify', 'nuxt-icons', 'nuxt-icon'],
-
     vuetify: {
         pluginOptions: {styles: true},
         vuetifyOptions: {
             blueprint: 'md3',
-            theme: {defaultTheme: 'dark'}
+            theme: {
+                defaultTheme: 'dark'
+            }
         }
     },
-
-    plugins: [
-        {src: '~/plugins/vercel.ts', mode: 'client'},
-        {src: '~/plugins/video-bg.client.ts', mode: 'client'}
-    ]
+    plugins: [{
+        src: '~/plugins/vercel.ts', mode: 'client'
+    },
+        {src: '~/plugins/video-bg.client.ts', mode: 'client'}]
 })
