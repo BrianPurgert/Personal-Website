2937dcefd48b0aa8d28097779a7a4dc763784e79
brian <brianpurgert2@gmail.com>
2023-10-01 03:59:40 -0400

    Does anyone read this? I'll be at the coffee shop accross the street.

--- components/BrianIsTyping.vue
+++ components/BrianIsTyping.vue
@@ -1,32 +1,32 @@
 <template>
 
-    <h2 class="text-h3 bp-shadow shadow-blue text-left">hello, my name is
-        <span class="text-caption">
+  <h2 class="text-h3 bp-shadow shadow-blue text-left">hello, my name is
+    <span class="text-caption">
             (Brian Purgert)
         </span>
-    </h2>
+  </h2>
 
-    <v-divider></v-divider>
-    <svg
-            class="logo" viewBox="0 0 100 17"
-    >
-        <symbol id="s-text">
-            <text class="b2" x="5" y="12">b</text>
-            <text class="b2" x="5" y="12">p</text>
-            <text class="" x="18" y="12">
-                {{ typeValue }}
-            </text>
+  <v-divider></v-divider>
+  <svg
+      class="logo" viewBox="0 0 100 17"
+  >
+    <symbol id="s-text">
+      <text class="b2" x="5" y="12">b</text>
+      <text class="b2" x="5" y="12">p</text>
+      <text class="" x="18" y="12">
+        {{ typeValue }}
+      </text>
 
-        </symbol>
-        <g class="g-ants">
-            <use class="text-copy" href="#s-text"></use>
-            <use class="text-copy" href="#s-text"></use>
-            <use class="text-copy" href="#s-text"></use>
-            <use class="text-copy" href="#s-text"></use>
-            <use class="text-copy" href="#s-text"></use>
-        </g>
-        <text class="b-top" x="5" y="12">b</text>
-    </svg>
+    </symbol>
+    <g class="g-ants">
+      <use class="text-copy" href="#s-text"></use>
+      <use class="text-copy" href="#s-text"></use>
+      <use class="text-copy" href="#s-text"></use>
+      <use class="text-copy" href="#s-text"></use>
+      <use class="text-copy" href="#s-text"></use>
+    </g>
+    <text class="b-top" x="5" y="12">b</text>
+  </svg>
 
 
 </template>
@@ -45,32 +45,32 @@ let displayTextArrayIndex = 0
 let charIndex = 0
 
 const typeText = () => {
-    if (charIndex < displayTextArray[displayTextArrayIndex].length) {
-        typeStatus.value = true
-        typeValue.value += displayTextArray[displayTextArrayIndex].charAt(charIndex)
-        charIndex++
-        setTimeout(typeText, typingSpeed)
-    } else {
-        typeStatus.value = false
-        setTimeout(eraseText, newTextDelay)
-    }
+  if (charIndex < displayTextArray[displayTextArrayIndex].length) {
+    typeStatus.value = true
+    typeValue.value += displayTextArray[displayTextArrayIndex].charAt(charIndex)
+    charIndex++
+    setTimeout(typeText, typingSpeed)
+  } else {
+    typeStatus.value = false
+    setTimeout(eraseText, newTextDelay)
+  }
 }
 
 const eraseText = () => {
-    if (charIndex > 0) {
-        typeStatus.value = true
-        typeValue.value = displayTextArray[displayTextArrayIndex].substring(0, charIndex - 1)
-        charIndex--
-        setTimeout(eraseText, erasingSpeed)
-    } else {
-        typeStatus.value = false
-        displayTextArrayIndex = (displayTextArrayIndex + 1) % displayTextArray.length
-        setTimeout(typeText, typingSpeed + 1000)
-    }
+  if (charIndex > 0) {
+    typeStatus.value = true
+    typeValue.value = displayTextArray[displayTextArrayIndex].substring(0, charIndex - 1)
+    charIndex--
+    setTimeout(eraseText, erasingSpeed)
+  } else {
+    typeStatus.value = false
+    displayTextArrayIndex = (displayTextArrayIndex + 1) % displayTextArray.length
+    setTimeout(typeText, typingSpeed + 1000)
+  }
 }
 
 onMounted(() => {
-    setTimeout(typeText, newTextDelay + 200)
+  setTimeout(typeText, newTextDelay + 200)
 })
 </script>
 
@@ -79,45 +79,45 @@ onMounted(() => {
 
 
 svg.logo {
-    align-content : baseline;
-    display       : block;
-    font-family   : 'tesla', cursive;
-    font-style    : italic;
-    max-height    : 9rem;
-    font-size     : 1rem;
-    margin        : 0 10%;
-    }
+  align-content: baseline;
+  display: block;
+  font-family: 'tesla', cursive;
+  font-style: italic;
+  max-height: 9rem;
+  font-size: 1rem;
+  margin: 0 10%;
+}
 
 .text-copy {
-    animation         : stroke-offset 10s infinite linear alternate;
-    fill              : #bd2c44;
-    stroke            : #2975a8;
-    stroke-dasharray  : 0 10 20;
-    stroke-dashoffset : -50%;
-    text-shadow       : 3px 3px 0px rgba(24, 24, 27, 1);
-    }
+  animation: stroke-offset 10s infinite linear alternate;
+  fill: #bd2c44;
+  stroke: #2975a8;
+  stroke-dasharray: 0 10 20;
+  stroke-dashoffset: -50%;
+  text-shadow: 3px 3px 0px rgba(24, 24, 27, 1);
+}
 
 .b-top {
-    fill : #bd2c44;
-    }
+  fill: #bd2c44;
+}
 
 .text-copy:nth-child(1) {
-    animation-delay : -1s;
-    }
+  animation-delay: -1s;
+}
 
 .text-copy:nth-child(2) {
-    animation-delay : -3s;
-    }
+  animation-delay: -3s;
+}
 
 .text-copy:nth-child(3) {
-    animation-delay : -5s;
-    }
+  animation-delay: -5s;
+}
 
 @keyframes stroke-offset {
-    100% {
-        stroke-dashoffset : 50%;
-        }
-    }
+  100% {
+    stroke-dashoffset: 50%;
+  }
+}
 
 
 </style>
--- components/MiamiTimelineItem.vue
+++ components/MiamiTimelineItem.vue
@@ -1,41 +1,41 @@
 <template>
-    <v-timeline-item>
-        <v-container style="min-height: 200px">
-            <v-card
+  <v-timeline-item>
+    <v-container style="min-height: 200px">
+      <v-card
 
-                    class="mx-auto align-center justify-center"
-                    width="600"
-            >
-                <v-card-item>
-                    <img src="/miami/logo-horizontal-stacked.png"
-                         class="pa-2"
-                         style=''/>
-                </v-card-item>
+          class="mx-auto align-center justify-center"
+          width="600"
+      >
+        <v-card-item>
+          <img src="/miami/logo-horizontal-stacked.png"
+               class="pa-2"
+               style=''/>
+        </v-card-item>
 
-                <v-card-title class="robot-crush text-center align-center">Computer Science & Computer Engineering
-                </v-card-title>
-                <v-card-text class="text-center align-center">
-                    <div class="text-h6">Miami University</div>
-                    <div class="text-caption">Oxford, OH</div>
-                </v-card-text>
+        <v-card-title class="robot-crush text-center align-center">Computer Science & Computer Engineering
+        </v-card-title>
+        <v-card-text class="text-center align-center">
+          <div class="text-h6">Miami University</div>
+          <div class="text-caption">Oxford, OH</div>
+        </v-card-text>
 
-            </v-card>
+      </v-card>
 
-        </v-container>
-        <template v-slot:opposite>
-            <div
-                    class="bp-year position-absolute mr-5"
-                    style="left: 0; margin: auto; height: 100px;"
+    </v-container>
+    <template v-slot:opposite>
+      <div
+          class="bp-year position-absolute mr-5"
+          style="left: 0; margin: auto; height: 100px;"
 
-            >2011 - 2016
-            </div>
-        </template>
-        <template v-slot:icon>
-        </template>
-    </v-timeline-item>
+      >2011 - 2016
+      </div>
+    </template>
+    <template v-slot:icon>
+    </template>
+  </v-timeline-item>
 </template>
 <script>
 export default {
-    name: 'miami-timeline-item'
+  name: 'miami-timeline-item'
 }
 </script>
