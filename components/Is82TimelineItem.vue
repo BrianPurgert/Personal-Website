a33a6dc2c7295908a5b29537ce883a418d296bed
BrianPurgert <brianpurgert2@gmail.com>
2024-02-02 01:27:03 -0500

    Fucking submodule bull shit

--- components/Is82TimelineItem.vue
+++ components/Is82TimelineItem.vue
@@ -1,60 +1,66 @@
-<template>
-  <v-timeline-item>
+<template >
+	  <v-timeline-item >
+			<template v-slot:default >
+				  <v-container >
+						<v-card
+							  id = "is82"
+							  class = "pa-2 ma-0"
 
-    <v-container>
-      <v-card
-          id="is82"
-          class="pa-2 ma-0"
-          width="600"
-      >
-        <v-carousel
-            continuous
-            cycle
-            hide-delimiters
-            hideDelimiterBackground
-            class="rounded"
-        >
-          <v-carousel-item
-              v-for="(item, index) in carouselItems"
-              :key="index"
-              cover
-              :src="`${imagePath}${item}`"
-          ></v-carousel-item>
+						>
+							  <v-carousel
+									continuous
+									cycle
+									hide-delimiters
+									hideDelimiterBackground
+									class = "rounded"
+							  >
+									<v-carousel-item
+										  v-for = "(item, index) in carouselItems"
+										  :key = "index"
+										  cover
+										  width = 'calc(100vw - 100px)'
+										  max-width = '600px'
+										  :src = "`${imagePath}${item}`"
+									></v-carousel-item >
 
-          <v-card-title>Mod Mapping with Battlecraft 1942</v-card-title>
-          <v-card-subtitle>images from 2004 map I created</v-card-subtitle>
-        </v-carousel>
+									<v-card-title >Mod Mapping with Battlecraft 1942</v-card-title >
+									<v-card-subtitle >images from 2004 map I created</v-card-subtitle >
+							  </v-carousel >
 
 
-      </v-card>
+						</v-card >
 
 
-    </v-container>
-    <template v-slot:opposite>
-      <div
-          class="bp-year"
-      >
-        12/yrs
-      </div>
-    </template>
-    <template v-slot:icon>
-      <img :src="`${imagePath}bf1942.png`" style='max-width: 100px'/>
-    </template>
-  </v-timeline-item>
-</template>
-<script setup>
-import {ref, computed} from 'vue';
+				  </v-container >
+			</template >
+			<template v-slot:opposite >
+				  <div
+						class = "bp-year"
 
-const imagePath = "/is82/"
+				  >
+						12/yrs
+				  </div >
+			</template >
+			<template v-slot:icon >
+				  <img :src = "`${imagePath}bf1942.png`"
+						class = ''
+						style = 'max-width: 100px' />
+			</template >
+	  </v-timeline-item >
+</template >
+<script setup >
+import { ref, computed } from "vue"
+
+const imagePath     = "/is82/"
 const carouselItems = ref([
-  'CIMG0367.JPG',
-  '2004_1.jpg',
-  '2004.jpg',
-  '2004_2.jpg',
-  'addict.jpg'
-])
+	                          "CIMG0367.JPG",
+	                          "2004_1.jpg",
+	                          "2004.jpg",
+	                          "2004_2.jpg",
+	                          "addict.jpg"
+                          ])
 
 const years = computed(
-    () => new Date().getFullYear() - 2004
+	() => new Date().getFullYear() - 2004
 )
-</script>
+</script >
--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,43 @@
-<script setup>
+<script setup >
 useSeoMeta({
-  description: '[description]',
-  ogTitle: '[og:title]',
-  ogDescription: '[og:description]',
-  ogImage: 'https://openclipart.org/image/800px/167281',
-  ogUrl: '[og:url]',
-  twitterTitle: '[twitter:title]',
-  twitterDescription: '[twitter:description]',
-  twitterImage: '[twitter:image]',
-  twitterCard: 'summary'
-})
+	           description       : "[description]",
+	           ogTitle           : "[og:title]",
+	           ogDescription     : "[og:description]",
+	           ogImage           : "https://openclipart.org/image/800px/167281",
+	           ogUrl             : "[og:url]",
+	           twitterTitle      : "[twitter:title]",
+	           twitterDescription: "[twitter:description]",
+	           twitterImage      : "[twitter:image]",
+	           twitterCard       : "summary"
+           })
 
 useHead({
-  htmlAttrs: {
-    lang: 'en'
-  },
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/png',
-      href: '/favicon.png'
-    }
-  ]
-})
-import CurrentProjects from "~/pages/CurrentProjects.vue";
+	        htmlAttrs: {
+		        lang: "en"
+	        },
+	        link     : [
+		        {
+			        rel : "icon",
+			        type: "image/png",
+			        href: "/favicon.png"
+		        }
+	        ]
+        })
+import CurrentProjects from "~/pages/CurrentProjects.vue"
 
 if (process.client) {
-  const consoleStyles = `color: greenyellow;font-size: 15px;`
-  console.log('%cSource Code: https://github.com/BrianPurgert/Personal-Website', consoleStyles)
+	const consoleStyles = `color: greenyellow;font-size: 15px;`
+	console.log("%cSource Code: https://github.com/BrianPurgert/Personal-Website", consoleStyles)
 }
+</script >
+
+<template >
+	  <v-card color = "default" variant = "flat" class = "bigname mx-auto pt-8 elevation-2 overflow-visible" >
+			<BrianIsTyping :displayTextArray = "['rian purgert   ','urgert ']" />
+	  </v-card >
+	  <projects-timeline />
+	  <currentProjects />
+	  <div class = "my-6" ></div >
 
 
-</script>
-
-<template>
-  <projects-timeline/>
-
-
-  <currentProjects/>
-  <div class="my-6"></div>
-
-
-</template>
+</template >
