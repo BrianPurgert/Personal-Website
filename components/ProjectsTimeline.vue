3f0f6c9d6177aecf6aaa7bc1d3a4ba3da6a7b13c
Brian Purgert <brian2@purgert.com>
2025-10-26 16:08:35 -0400

    Add descriptive notes to timeline items in ProjectsTimeline component. Update IntelliJ configuration files.

--- components/ProjectsTimeline.vue
+++ components/ProjectsTimeline.vue
@@ -12,13 +12,13 @@
 	  >
 
 			<!--    2004 - 12 Battlefield 1942 modding IS82	-->
-			<is82-timeline-item note='IS82 was a sick mod for Battlefield 1942, I mostly just played around with the map editor and installed maps'/>
+			<is82-timeline-item note='IS82 was a sick mod for Battlefield 1942, Brian mostly just played around with the map editor and installed maps'/>
 			<!--	2006 - 14 Runescape Scripting  	-->
-			<runescape-timeline-item />
+			<runescape-timeline-item note="Brians introduction to programming was botting in RuneScape, yes he did get banned."/>
 			<!--    2008 - 16 Crysis Interstate   -->
-			<cryengine-timeline-item />
+			<cryengine-timeline-item note='Brian recreated deathrace from the battlefield mod in cryengine.'/>
 			<!--	2010 - 18 DoodleBike   -->
-			<doodlebike-timeline-item />
+			<doodlebike-timeline-item note='Brian published doodlebike to the playstore.'/>
 			<!--	2010 - 18 SpaceBike	-->
 			<spacebike-timeline-item />
 			<!--	2011 - 19	-->
